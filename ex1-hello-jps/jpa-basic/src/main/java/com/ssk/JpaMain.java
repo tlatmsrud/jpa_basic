d8deab6c02068148216b711cda7b436f7417b5bc
tlatmsrud <tlatmsrud@naver.com>
2022-06-01 22:45:07 +0900

    [Update/심승경] Lecture Note, 양방향 연관관계, 주인

--- ex1-hello-jps/jpa-basic/src/main/java/com/ssk/JpaMain.java
+++ ex1-hello-jps/jpa-basic/src/main/java/com/ssk/JpaMain.java
@@ -1,5 +1,7 @@
 package com.ssk;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -25,23 +27,25 @@ public class JpaMain {
 			Team team = new Team();
 			team.setName("TeamA");
 			
-			System.out.println("1------------------");
 			//team을 영속성 컨텍스트에 저장
 			//team에 대한 id 값은 영속성 컨텍스트에 저장될 시점에 쿼리를 날려 가져옴.
 			em.persist(team);
-			System.out.println("2------------------");
 			Member member = new Member();
 			member.setUsername("member1");
 			member.setTeam(team);
-			System.out.println("3------------------");
 			em.persist(member);
 			
-			System.out.println("4------------------");
+			em.flush();
+			em.clear();
 			//member.getId로 조회하면 영속성 컨텍스트에 들어가 있는 member를 가져옴.
 			Member findMember = em.find(Member.class, member.getId());
-			System.out.println("5------------------");
-			Team findTeam = findMember.getTeam();
-			System.out.println(findTeam.getName());
+
+			List<Member> members = findMember.getTeam().getMembers();
+			
+			for(Member m : members) {
+				System.out.println("m = "+ m.getUsername());
+			}
+
 			
 			//Team, Member Insert 쿼리 실행
 			tx.commit();
--- ex1-hello-jps/jpa-basic/src/main/java/com/ssk/Team.java
+++ ex1-hello-jps/jpa-basic/src/main/java/com/ssk/Team.java
@@ -1,9 +1,13 @@
 package com.ssk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Team {
@@ -17,6 +21,9 @@ public class Team {
 	
 	@Column(name = "NAME")
 	private String name;
+	
+	@OneToMany(mappedBy="team")
+	private List<Member> members = new ArrayList<Member>();
 
 	public long getId() {
 		return id;
@@ -32,6 +39,14 @@ public class Team {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<Member> getMembers() {
+		return members;
+	}
+
+	public void setMembers(List<Member> members) {
+		this.members = members;
 	}
 	
 	
